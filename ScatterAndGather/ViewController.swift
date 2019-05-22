04bacf96e0a51b2993bd0eecb0a4d2d00a02d32b
Kobe McKee <kobe.mckee@gmail.com>
2019-05-22 12:52:42 -0600

    initial UI and outlet setup

--- ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isScattered: Bool = false
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var a1Label: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    @IBOutlet weak var a2Label: UILabel!
+    
+    @IBOutlet weak var logoImageView: UIImageView!
 }
 
